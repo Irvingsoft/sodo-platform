b6cc2dc237a9a6a34ec058b7e4decbfac61741a7
TimeChaser <1872101334@qq.com>
2021-09-10 13:54:49 +0800

    注解化分布式锁完善

--- sodo-common/sodo-redis-starter/src/main/java/cool/sodo/redis/aspect/LockAspect.java
+++ sodo-common/sodo-redis-starter/src/main/java/cool/sodo/redis/aspect/LockAspect.java
@@ -1,24 +1,16 @@
 package cool.sodo.redis.aspect;
 
-import cool.sodo.common.base.component.RedisCacheHelper;
-import cool.sodo.common.base.entity.Constants;
 import cool.sodo.common.base.entity.ResultEnum;
 import cool.sodo.common.base.exception.SoDoException;
-import cool.sodo.common.base.util.StringPool;
 import cool.sodo.common.base.util.StringUtil;
 import cool.sodo.redis.annotation.Lock;
+import cool.sodo.redis.component.LockComponent;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.core.DefaultParameterNameDiscoverer;
-import org.springframework.expression.Expression;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author TimeChaser
@@ -28,10 +20,8 @@ import java.util.concurrent.TimeUnit;
 @Aspect
 public class LockAspect {
 
-    private final SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
-    private final DefaultParameterNameDiscoverer nameDiscoverer = new DefaultParameterNameDiscoverer();
     @Resource
-    private RedisCacheHelper redisCacheHelper;
+    private LockComponent lockComponent;
 
     @Around(value = "@within(lock) || @annotation(lock)")
     public Object process(ProceedingJoinPoint point, Lock lock) throws Throwable {
@@ -42,23 +32,13 @@ public class LockAspect {
         if (StringUtil.isEmpty(lock)) {
             throw new SoDoException(ResultEnum.SERVER_ERROR, "分布式锁为空！");
         }
-        String lockKey = lock.key();
-        if (StringUtil.isEmpty(lockKey)) {
-            throw new SoDoException(ResultEnum.SERVER_ERROR, "分布式锁的 Key 为空！");
-        }
-        if (lockKey.contains(StringPool.HASH)) {
-            lockKey = getValueBySpEl(point, lockKey);
-            if (StringUtil.isEmpty(lockKey)) {
-                throw new SoDoException(ResultEnum.SERVER_ERROR, "分布式锁的 Key 为空！");
-            }
-        }
-
-        Boolean lockUp = false;
+        boolean lockUp = false;
+        String lockKey = lockComponent.getKey(lock.key(), point);
         try {
             if (lock.waitTime() > 0) {
-                lockUp = tryLock(lockKey, lock.waitTime(), lock.leaseTime(), lock.timeUnit());
+                lockUp = lockComponent.tryLock(lockKey, lock.waitTime(), lock.leaseTime(), lock.timeUnit());
             } else {
-                lockUp = tryLock(lockKey, lock.leaseTime(), lock.timeUnit());
+                lockUp = lockComponent.tryLock(lockKey, lock.leaseTime(), lock.timeUnit());
             }
             if (lockUp) {
                 return point.proceed();
@@ -67,42 +47,8 @@ public class LockAspect {
             }
         } finally {
             if (lockUp) {
-                unlock(lockKey);
+                lockComponent.unlock(lockKey);
             }
         }
-    }
-
-    private String getValueBySpEl(ProceedingJoinPoint point, String spEl) {
-
-        String[] parameterNames = nameDiscoverer.getParameterNames(((MethodSignature) point.getSignature()).getMethod());
-        Object[] args = point.getArgs();
-        if (!StringUtil.isEmpty(parameterNames)) {
-            Expression expression = spelExpressionParser.parseExpression(spEl);
-            StandardEvaluationContext context = new StandardEvaluationContext();
-            for (int i = 0; i < args.length; i++) {
-                context.setVariable(parameterNames[i], args[i]);
-            }
-            return expression.getValue(context, String.class);
-        }
-        return null;
-    }
-
-    private Boolean tryLock(String key, Long waitTime, Long leastTime, TimeUnit timeUnit) {
-
-        long startTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() - startTime > timeUnit.toMillis(waitTime)) {
-            if (redisCacheHelper.setIfAbsent(Constants.LOCK_PREFIX + key, key, leastTime, timeUnit)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private Boolean tryLock(String key, Long leastTime, TimeUnit timeUnit) {
-        return redisCacheHelper.setIfAbsent(Constants.LOCK_PREFIX + key, key, leastTime, timeUnit);
-    }
-
-    private void unlock(String key) {
-        redisCacheHelper.delete(Constants.LOCK_PREFIX + key);
     }
 }
---
+++ sodo-common/sodo-redis-starter/src/main/java/cool/sodo/redis/component/LockComponent.java
@@ -0,0 +1,79 @@
+package cool.sodo.redis.component;
+
+import cool.sodo.common.base.component.RedisCacheHelper;
+import cool.sodo.common.base.entity.Constants;
+import cool.sodo.common.base.entity.ResultEnum;
+import cool.sodo.common.base.exception.SoDoException;
+import cool.sodo.common.base.util.StringPool;
+import cool.sodo.common.base.util.StringUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author TimeChaser
+ * @date 2021/9/10 11:24
+ */
+@Component
+public class LockComponent {
+
+    private final SpelExpressionParser spelExpressionParser = new SpelExpressionParser();
+    private final DefaultParameterNameDiscoverer nameDiscoverer = new DefaultParameterNameDiscoverer();
+
+    @Resource
+    private RedisCacheHelper redisCacheHelper;
+
+    public String getKey(String key, ProceedingJoinPoint point) {
+        if (StringUtil.isEmpty(key)) {
+            throw new SoDoException(ResultEnum.SERVER_ERROR, "分布式锁的 Key 为空！");
+        }
+        if (key.contains(StringPool.HASH)) {
+            key = getValueBySpEl(key, point);
+            if (StringUtil.isEmpty(key)) {
+                throw new SoDoException(ResultEnum.SERVER_ERROR, "分布式锁的 Key 为空！");
+            }
+        }
+        return key;
+    }
+
+    public String getValueBySpEl(String spEl, ProceedingJoinPoint point) {
+
+        String[] parameterNames = nameDiscoverer.getParameterNames(((MethodSignature) point.getSignature()).getMethod());
+        Object[] args = point.getArgs();
+        if (!StringUtil.isEmpty(parameterNames)) {
+            Expression expression = spelExpressionParser.parseExpression(spEl);
+            StandardEvaluationContext context = new StandardEvaluationContext();
+            for (int i = 0; i < args.length; i++) {
+                context.setVariable(parameterNames[i], args[i]);
+            }
+            return expression.getValue(context, String.class);
+        }
+        return null;
+    }
+
+    public Boolean tryLock(String key, Long waitTime, Long leastTime, TimeUnit timeUnit) {
+
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < timeUnit.toMillis(waitTime)) {
+            if (redisCacheHelper.setIfAbsent(Constants.LOCK_PREFIX + key, key, leastTime, timeUnit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean tryLock(String key, Long leastTime, TimeUnit timeUnit) {
+        return redisCacheHelper.setIfAbsent(Constants.LOCK_PREFIX + key, key, leastTime, timeUnit);
+    }
+
+    public void unlock(String key) {
+        redisCacheHelper.delete(Constants.LOCK_PREFIX + key);
+    }
+}
